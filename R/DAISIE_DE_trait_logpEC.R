4cfd2a74f1f4a65bd875409ff5adaeb379c4801c
Ornela Dehayem Nanwou <o.dehayem@gmail.com>
2025-08-28 15:38:34 +0200

    update example

--- R/DAISIE_DE_trait_logpEC.R
+++ R/DAISIE_DE_trait_logpEC.R
@@ -16,7 +16,7 @@
 #' i <- 4
 #' phy <- DDD::brts2phylo(datalist[[i]]$branching_times[-c(1, 2)])
 #' brts <- datalist[[i]]$branching_times
-#' traits <- sample(c(0, 0), length(brts), replace = TRUE)
+#' traits <- sample(c(NA, NA), length(brts), replace = TRUE)
 #' sampling_fraction <- sample(c(1, 1), length(brts), replace = TRUE)
 #'
 #'
